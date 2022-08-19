ec64fadc085b4c50ecba6a9eee824526e99945a1
John Leonne <johnleonne@hotmail.com>
2022-08-19 19:25:54 -0400

    feat: adicionado atributos na tela de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -7,6 +7,30 @@ export default function Profile() {
     <main>
       <Header title="Profile" />
       <h1>Profile page</h1>
+      <h3 data-testid="profile-email">email@email.com</h3>
+
+      <div>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          Done Recipes
+        </button>
+
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Favorite Recipes
+        </button>
+
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </main>
   );
