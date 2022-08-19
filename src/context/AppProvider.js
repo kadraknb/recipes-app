257f2f4b9141594a8199cd6daa36610aaaccc30d
Gustavo <guvono4@gmail.com>
2022-08-19 15:39:14 -0300

    configurando context

---
+++ src/context/AppProvider.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { node } from 'prop-types';
+import AppContext from './AppContext';
+
+function AppProvider({ children }) {
+  return (
+    <AppContext.Provider
+      value={ {
+
+      } }
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+AppProvider.propTypes = {
+  children: node,
+}.isRequired;
+
+export default AppProvider;
