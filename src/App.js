bb205f797e7c407c9fedcfb5f2d8e89e1a7eeee6
John Leonne <johnleonne@hotmail.com>
2022-08-19 16:45:38 -0400

    feat: Adição de rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { foods, Profile, Drinks, DoneRecipes, FavoriteRecipes } from './pages';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo"> TRYBE </span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+
+    <Switch>
+
+      <Route exact path="/foods" component={ foods } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+      <Route exact path="/profile" component={ Profile } />
+
+    </Switch>
+
   );
 }
 
