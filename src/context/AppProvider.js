3747832acde79950a924cee2b25d27e129cfbf8c
Itamar da Silva Soares <Itamar_Soares@zohomail.com>
2022-08-22 18:39:26 -0300

    pagina de recipes concluído e passando nos testes

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -3,12 +3,38 @@ import { node } from 'prop-types';
 import AppContext from './AppContext';
 
 function AppProvider({ children }) {
+  // component Recipes
+  const [recipesMeals, setRecipesMeals] = useState([]);
+  const [filterControl, setFilterControl] = useState({
+    id: '',
+    filterWithCategory: false,
+  });
+  const [recipesDrinks, setRecipesDrinks] = useState([]);
+  const [categoriesMeals, setCategoriesMeals] = useState([]);
+  const [categoriesDrinks, setCategoriesDrinks] = useState([]);
+
+  // page Login
   const [user, setUser] = useState({
     email: '',
     password: '',
   });
   const [toggleBtnLogin, setToggleBtnLogin] = useState(true);
-  const value = { user, setUser, toggleBtnLogin, setToggleBtnLogin };
+
+  const value = { user,
+    setUser,
+    toggleBtnLogin,
+    setToggleBtnLogin,
+    recipesMeals,
+    setRecipesMeals,
+    recipesDrinks,
+    setRecipesDrinks,
+    categoriesMeals,
+    setCategoriesMeals,
+    categoriesDrinks,
+    setCategoriesDrinks,
+    filterControl,
+    setFilterControl,
+  };
   return (
     <AppContext.Provider
       value={ value }
