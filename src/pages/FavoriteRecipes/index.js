166781df6071cf15619a6e0558ffb7792d193a05
John Leonne <johnleonne@hotmail.com>
2022-08-25 11:12:55 -0400

    update: favorites page

--- src/pages/FavoriteRecipes/index.js
+++ src/pages/FavoriteRecipes/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import DetailFavoriteCard from '../../components/DetailFavoriteCard';
 import Header from '../../components/Header';
+import AppContext from '../../context/AppContext';
 
 export default function FavoriteRecipes() {
+  const [filter, setFilter] = useState('');
+  const { favorites } = useContext(AppContext);
+
+  const options = [
+    { name: 'all', value: '' },
+    { name: 'food', value: 'food' },
+    { name: 'drink', value: 'drink' },
+  ];
+
+  const handleChange = ({ target: { value } }) => {
+    setFilter(value);
+  };
+
+  const display = favorites.filter(({ type }) => type.includes(filter));
+
   return (
-    <main className="">
+    <>
       <Header title="Favorite Recipes" />
-      <h1>Favorite Recipes page</h1>
-    </main>
+      <main id="favoriterecipes-page">
+        <form className="formContainer">
+          {options.map(({ name, value }) => (
+            <label
+              key={ name }
+              data-testid={ `filter-by-${name}-btn` }
+              htmlFor={ `filter-by-${name}-btn` }
+            >
+              <input
+                id={ `filter-by-${name}-btn` }
+                type="radio"
+                name="filter"
+                checked={ filter === value }
+                value={ value }
+                onChange={ handleChange }
+              />
+              {name}
+            </label>
+          ))}
+        </form>
+        <section className="favoritesContainer">
+          {display.map((obj, index) => (
+            <DetailFavoriteCard
+              { ...obj }
+              index={ index }
+              key={ index }
+            />
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
